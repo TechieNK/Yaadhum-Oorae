3197e53d945cf741ab093861f301e700986392ff
Xavier Emmanuel E <xavier12emmanuel@gmail.com>
2020-10-30 01:44:04 +0530

    Created the Login.java and created the instance for the authentication.

--- app/src/main/java/project/Yaadhum_Oorae/Activity/Login.java
+++ app/src/main/java/project/Yaadhum_Oorae/Activity/Login.java
@@ -3,12 +3,20 @@ package project.Yaadhum_Oorae.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        fAuth = FirebaseAuth.getInstance();
     }
 }
