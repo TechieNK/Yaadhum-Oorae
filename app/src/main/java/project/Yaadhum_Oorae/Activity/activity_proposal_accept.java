d1f7ddef1dc4b26f41e52873852b6c51ce984d3e
peeyon <techiepeeyon@gmail.com>
2020-10-30 11:38:57 -0700

    Factory Details page is over

---
+++ app/src/main/java/project/Yaadhum_Oorae/Activity/activity_proposal_accept.java
@@ -0,0 +1,14 @@
+package project.Yaadhum_Oorae.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_proposal_accept extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proposal_accept);
+    }
+}
---
+++ app/src/main/java/project/Yaadhum_Oorae/Activity/activity_vendor.java
@@ -0,0 +1,14 @@
+package project.Yaadhum_Oorae.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_vendor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor);
+    }
+}
